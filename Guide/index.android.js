728325aa78bec8196335ee08d3837ac28706bf33
qustdjx <1099629968@qq.com>
2017-01-09 22:25:19 +0800

    添加引导程序

--- Guide/index.android.js
+++ Guide/index.android.js
@@ -4,50 +4,116 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+/**
+ * HelloWorld1 Native App
+ * https://github.com/facebook/react-native
+ * @author:lidong
+ * @date：2016-02-28
+ */
+'use strict';
+import React,{ Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    Navigator,
+    TouchableHighlight,
+    TouchableOpacity,
+    ToastAndroid,
+    ViewPagerAndroid,
+    Image,
+    View
 } from 'react-native';
 
-export default class Guide extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+
+class NavButton extends React.Component {
+
+    render() {
+        return (
+            <TouchableHighlight
+        style={styles.button}
+        underlayColor="#B5B5B5"
+        onPress={this.props.onPress}>
+    <Text style={styles.buttonText}>{this.props.text}</Text>
+        </TouchableHighlight>
     );
-  }
+    }
 }
 
+class Guide extends Component {
+
+    render(){
+        return (
+            <ViewPagerAndroid
+        style={styles.viewPager}
+        initialPage={0}>
+            <View style={styles.pageStyle} >
+    <Image source={require('./img/p1.png')}
+        style={styles.image}
+    />
+    </View>
+        <View style={styles.pageStyle}>
+    <Image source={require('./img/p2.png')}
+        style={styles.image}
+    />
+    </View>
+        <View style={styles.pageStyle}>
+    <Image source={require('./img/p3.png')}
+        style={styles.image} >
+    <NavButton
+        onPress={() => {
+            ToastAndroid.show('启动应用', ToastAndroid.SHORT);
+
+        }}
+        text="启动应用"
+        style={styles.button1}  />
+    </Image>
+        </View>
+        </ViewPagerAndroid>
+    );
+    }
+};
+
+//React Native组件的生命周期，经历了Mount->Update->Unmount这三个大的过程，即从创建到销毁的过程
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    viewPager: {
+        flex: 1,
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    pageStyle: {
+        flex: 1,
+        alignItems: 'stretch',
+        justifyContent: 'center',
+        position:'relative',
+    },
+    image: {
+        flex: 1,
+        justifyContent:'flex-end',
+        alignItems:'center',
+        paddingBottom:50,
+    },
+    button1:{
+        width:200,
+        alignSelf: 'flex-end',
+        justifyContent:'flex-end',
+    },
+    messageText: {
+        fontSize: 27,
+        fontWeight: '500',
+        padding: 15,
+        marginTop: 50,
+        marginLeft: 15,
+    },
+    button: {
+        backgroundColor: 'white',
+        padding: 15,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomColor: '#CDCDCD',
+        alignSelf:'center',
+    },
 });
 
 AppRegistry.registerComponent('Guide', () => Guide);
